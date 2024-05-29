881f68a842d967aaa4da06b72370c22bcfd0e4ae
sessaadouni <essaadouni.s@gmail.com>
2024-05-29 16:34:45 +0200

    udpate the Event Schema DB and the controllor_2024-05-29 16:34:45

--- app/controllers/Event_controller.py
+++ app/controllers/Event_controller.py
@@ -21,51 +21,49 @@ class EventController:
   
   @app.route('/event', methods=['POST'])
   def create_event():
-    #name = request.json['name']
-    eventName = request.json['eventName']
-    eventStart = request.json['eventStart']
-    eventEnd = request.json['eventEnd']
-    eventLocation = request.json['eventLocation']
-    eventDescription = request.json['eventDescription']
-    is_executed = request.json['is_executed']
-
-    # verifier si le nom de la tâche est vide
+    data = request.json
+    eventName = data.get('eventName')
     if not eventName: return jsonify({"error": "Le nom de la tâche est obligatoire"}), 400
     
-    # verifier si le nom de la tâche est déjà utilisé
-    event = EventItem.query.filter_by(eventName=eventName).first()
-    if event: return jsonify({"error": "Cette tâche existe déjà"}), 400
-    #new_event_item = EventItem(name, is_executed)
-    new_event_item = EventItem(  
-                               eventName, 
-                               eventStart, 
-                               eventEnd, 
-                               eventLocation, 
-                               eventDescription,
-                               is_executed
-                               )
+    if EventItem.query.filter_by(eventName=eventName).first(): return jsonify({"error": "Cette tâche existe déjà"}), 400
+
+    new_event_item = EventItem(
+      eventName,
+      data.get('eventStart'),
+      data.get('eventEnd'),
+      data.get('eventLocation'),
+      data.get('eventDescription'),
+      data.get('is_executed')
+    )
     db.session.add(new_event_item)
     db.session.commit()
 
-    return Event_schema.jsonify(new_event_item)
+    return Event_schema.jsonify(new_event_item), 201
   
   @app.route('/event/<id>', methods=['PUT', 'PATCH'])
   def update_event(id):
     event = EventItem.query.get(id)
+    if not event: return jsonify({"error": "La tâche n'existe pas"}), 404
     
-    #if request.json['name']: name = request.json['name']
-    if request.json['eventName']: eventName = request.json['eventName']
-    if request.json['eventStart']: eventStart = request.json['eventStart']
-    if request.json['eventEnd']: eventEnd = request.json['eventEnd']
-    if request.json['eventLocation']: eventLocation = request.json['eventLocation']
-    if request.json['eventDescription']: eventDescription = request.json['eventDescription']
-    if request.json['is_executed']: is_executed = request.json['is_executed']
-    pass
+    data = request.json
+    if 'eventName' in data:
+      if EventItem.query.filter_by(eventName=data['eventName']).first(): return jsonify({"error": "Cette évenement existe déjà"}), 400
+      event.eventName = data['eventName']
+    
+    event.eventStart = data.get('eventStart', event.eventStart)
+    event.eventEnd = data.get('eventEnd', event.eventEnd)
+    event.eventLocation = data.get('eventLocation', event.eventLocation)
+    event.eventDescription = data.get('eventDescription', event.eventDescription)
+    event.is_executed = data.get('is_executed', event.is_executed)
 
-  @app.route('/event/<id>', methods=['PUT', 'PATCH'])
+    db.session.commit()
+    return jsonify({"success": "La tâche a été modifiée"})
+
+  @app.route('/event/<id>/execute', methods=['PATCH'])
   def execute_event(id):
     event = EventItem.query.get(id)
     if not event: return jsonify({"error": "La tâche n'existe pas"}), 404
+    
     event.is_executed = not event.is_executed
     db.session.commit()
     return Event_schema.jsonify(event)
@@ -74,7 +72,7 @@ class EventController:
   def delete_event(id):
     event_to_delete = EventItem.query.get(id)
     if not event_to_delete: return jsonify({"error": "La tâche n'existe pas"}), 404
+    
     db.session.delete(event_to_delete)
     db.session.commit()
-    # retourner un message de succès
-    return jsonify({"success": "La tâche a été supprimée"})
+    return jsonify({"success": "La tâche a été supprimée"}), 204
