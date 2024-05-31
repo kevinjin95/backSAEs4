3f1de34fed415835918a690496db714cc8ac0109
Yasmine <yasmine.mankour.sio@gmail.com>
2024-06-01 00:06:27 +0200

    ajout des event Year/Day/Month

--- app/models/Event_model.py
+++ app/models/Event_model.py
@@ -4,15 +4,21 @@ class Event(db.Model):
   id = db.Column(db.Integer, primary_key=True)
   eventName = db.Column(db.String(100))
   eventStart = db.Column(db.String(100))
-  eventEnd = db.Column(db.String(100))
+  eventEnd = db.Column(db.String(100)) #ajt des toris col regarder structure int
   eventLocation = db.Column(db.String(100))
   eventDescription = db.Column(db.String(100))
+  eventYear = db.Column(db.Integer)
+  eventMonth = db.Column(db.Integer)
+  eventDay = db.Column(db.Integer)
   is_executed = db.Column(db.Boolean)
 
-  def __init__(self, eventName, eventStart, eventEnd, eventLocation, eventDescription, is_executed):
-    self.is_executed = is_executed
+  def __init__(self, eventName, eventStart, eventEnd, eventLocation, eventDescription, eventYear, eventMonth, eventDay, is_executed):#ajt trois col
     self.eventName = eventName
     self.eventStart = eventStart
     self.eventEnd = eventEnd
     self.eventLocation = eventLocation
     self.eventDescription = eventDescription
+    self.eventYear = eventYear
+    self.eventMonth = eventMonth
+    self.eventDay = eventDay
+    self.is_executed = is_executed
